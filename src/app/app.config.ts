0014103aefa3d220e88c27f6683421b246e18bb2
Moaz <moaz_laptop@remotly.com>
2025-01-27 20:24:19 +0200

    [+] install primeng

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,28 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+/**
+ * libs
+ */
+import Aura from '@primeng/themes/aura';
+import { providePrimeNG } from 'primeng/config';
+
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideAnimationsAsync(),
+    providePrimeNG({
+        ripple: true,
+        theme: {
+          preset: Aura,
+          options: {
+            darkModeSelector: false
+          }
+        }
+    }),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
   ]
