a3c173ce1c2d0082b1110a6ade28d894c8dd8233
Moaz <moaz_laptop@remotly.com>
2025-01-27 07:22:05 +0200

    [+] create InvoiceManagementComponent

---
+++ src/app/modules/system/invoice-management/invoice-management.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-invoice-management',
+  imports: [],
+  templateUrl: './invoice-management.component.html',
+  styleUrl: './invoice-management.component.scss'
+})
+export class InvoiceManagementComponent {
+
+}
