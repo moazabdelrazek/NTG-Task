371742fa7fb3aa578a8a836b2ad04cb0e998eaa7
Moaz <moaz_laptop@remotly.com>
2025-01-27 18:34:26 +0200

    [+=] handler badge types

--- src/app/modules/shared/badge/badge.component.ts
+++ src/app/modules/shared/badge/badge.component.ts
@@ -1,6 +1,11 @@
 import { NgClass } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
+/**
+ * enums
+ */
+import { BADGE_TYPE } from './enums/badge-type';
+
 @Component({
   selector: 'app-badge',
   templateUrl: './badge.component.html',
@@ -12,6 +17,31 @@ export class BadgeComponent {
   /**
    * @description ClassList to set class value
    */
-  @Input({ alias: "classList" }) public Class: string = "";  
+  @Input({ alias: "classList" }) public Class: string = "";
+
+  /**
+   * @description to defined which ui that will show
+   */
+  @Input({ alias: "type" }) public Type!: BADGE_TYPE
+
+  /**
+   * @description return css class depend on type value
+   */
+  public get GetBadgeType(): string | null {
+
+    switch(this.Type) {
+      case BADGE_TYPE.Primary:
+        return "primary";
+      case BADGE_TYPE.Secondary:
+        return "secondary";
+      case BADGE_TYPE.Success:
+        return "success";
+      case BADGE_TYPE.Warning:
+        return "warning";
+      default:
+        return ""
+    }
+
+  }
 
 }
---
+++ src/app/modules/shared/badge/enums/badge-type.ts
@@ -0,0 +1,6 @@
+export enum BADGE_TYPE {
+    Primary = 1,
+    Secondary = 2,
+    Success = 3,
+    Warning = 4
+}
